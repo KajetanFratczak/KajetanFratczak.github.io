05777f722c9b5aa3ad95661a501d46dcd3cb11cf
Kajetan Frątczak <kfratczak@student.agh.edu.pl>
2024-11-04 17:44:38 +0100

    Prawie...
    prawie...

--- script.js
+++ script.js
@@ -4,22 +4,60 @@ function getRandomItem(arr) {
   return item;
 }
 
+// Funkcja wybierająca ćwiczenia dla konkretnej partii mięśniowej
+function addExercisesForMuscleGroup(dzien, partia, rep_range, ciezar, counter) {
+  const partia_miesniowa = exercises.find((group) => group.name === partia);
+
+  if (partia_miesniowa) {
+    for (let i = 0; i < counter; i++) {
+      const nazwa_cwiczenia = getRandomItem(partia_miesniowa.exercises);
+      dzien.exercises.push({
+        nazwa: nazwa_cwiczenia,
+        reps: rep_range,
+        waga: ciezar,
+      });
+    }
+  } else {
+    console.error("Nieznana partia mięśniowa:", partia);
+  }
+}
+
 function generatePlan() {
   const poziom_zaaw = document.getElementById("poziom_zaaw").value;
   const cel = document.getElementById("cel").value;
   const plan_generator = document.getElementById("plan_generator");
 
   let liczba_dni = 0;
+  let muscle_groups = [];
   if (poziom_zaaw === "poczatkujacy") {
     liczba_dni = 3;
+    muscle_groups = [
+      { group: "Klatka i Triceps" },
+      { group: "Biceps i Plecy" },
+      { group: "Barki i Nogi" },
+    ];
   } else if (poziom_zaaw === "srednio_zaawansowany") {
     liczba_dni = 4;
+    muscle_groups = [
+      { group: "Klatka i Triceps" },
+      { group: "Nogi" },
+      { group: "Barki i Biceps" },
+      { group: "Plecy i Brzuch" },
+    ];
   } else {
     liczba_dni = 5;
+    muscle_groups = [
+      { group: "Nogi" },
+      { group: "Klatka i Triceps" },
+      { group: "Plecy" },
+      { group: "Barki" },
+      { group: "Biceps i Brzuch" },
+    ];
   }
 
   const workout_plan = [];
   if (cel == "sila") {
+    let workout_day = 0;
     const ciezar = "80% max ciezaru";
     const rep_range = "6 - 8";
     for (let day = 1; day <= 7; day++) {
@@ -30,24 +68,44 @@ function generatePlan() {
       ) {
         workout_plan.push({ day: `Dzień ${day}`, type: "Rest Day" });
       } else {
-        const partia_miesniowa = getRandomItem(exercises);
+        const partie = muscle_groups[workout_day].group;
         const dzien = {
-          day: `Dzień ${day}`,
+          day: `Dzień ${day} ${partie}`,
           exercises: [],
           type: "Workout Day",
         };
-        for (let i = 0; i < 4; i++) {
-          const nazwa_cwiczenia = getRandomItem(partia_miesniowa.exercises);
-          dzien.exercises.push({
-            nazwa: nazwa_cwiczenia,
-            reps: rep_range,
-            waga: ciezar,
-          });
+
+        // Dodaj ćwiczenia na podstawie partii mięśniowej
+        if (partie.includes("Biceps"))
+          addExercisesForMuscleGroup(dzien, "biceps", rep_range, ciezar, 3);
+        if (partie.includes("Triceps"))
+          addExercisesForMuscleGroup(dzien, "triceps", rep_range, ciezar, 3);
+        if (partie.includes("Nogi")) {
+          addExercisesForMuscleGroup(dzien, "quadriceps", rep_range, ciezar, 1);
+          addExercisesForMuscleGroup(
+            dzien,
+            "hamstring_glutes",
+            rep_range,
+            ciezar,
+            1
+          );
+          addExercisesForMuscleGroup(dzien, "calves", rep_range, ciezar, 1);
         }
+        if (partie.includes("Plecy"))
+          addExercisesForMuscleGroup(dzien, "back", rep_range, ciezar, 3);
+        if (partie.includes("Brzuch"))
+          addExercisesForMuscleGroup(dzien, "abs", rep_range, ciezar, 3);
+        if (partie.includes("Klatka"))
+          addExercisesForMuscleGroup(dzien, "chest", rep_range, ciezar, 3);
+        if (partie.includes("Barki"))
+          addExercisesForMuscleGroup(dzien, "shoulders", rep_range, ciezar, 3);
+
         workout_plan.push(dzien);
+        workout_day++;
       }
     }
   } else if (cel == "wytrzymalosc") {
+    let workout_day = 0;
     const ciezar = "60% max ciezaru";
     const rep_range = "10 - 12";
     for (let day = 1; day <= 7; day++) {
@@ -58,21 +116,40 @@ function generatePlan() {
       ) {
         workout_plan.push({ day: `Dzień ${day}`, type: "Rest Day" });
       } else {
-        const partia_miesniowa = getRandomItem(exercises);
+        const partie = muscle_groups[workout_day].group;
         const dzien = {
-          day: `Dzień ${day}`,
+          day: `Dzień ${day} ${partie}`,
           exercises: [],
           type: "Workout Day",
         };
-        for (let i = 0; i < 5; i++) {
-          const nazwa_cwiczenia = getRandomItem(partia_miesniowa.exercises);
-          dzien.exercises.push({
-            nazwa: nazwa_cwiczenia,
-            reps: rep_range,
-            waga: ciezar,
-          });
+
+        // Dodaj ćwiczenia na podstawie partii mięśniowej
+        if (partie.includes("Biceps"))
+          addExercisesForMuscleGroup(dzien, "biceps", rep_range, ciezar, 3);
+        if (partie.includes("Triceps"))
+          addExercisesForMuscleGroup(dzien, "triceps", rep_range, ciezar, 3);
+        if (partie.includes("Nogi")) {
+          addExercisesForMuscleGroup(dzien, "quadriceps", rep_range, ciezar, 1);
+          addExercisesForMuscleGroup(
+            dzien,
+            "hamstring_glutes",
+            rep_range,
+            ciezar,
+            1
+          );
+          addExercisesForMuscleGroup(dzien, "calves", rep_range, ciezar, 1);
         }
+        if (partie.includes("Plecy"))
+          addExercisesForMuscleGroup(dzien, "back", rep_range, ciezar, 3);
+        if (partie.includes("Brzuch"))
+          addExercisesForMuscleGroup(dzien, "abs", rep_range, ciezar, 3);
+        if (partie.includes("Klatka"))
+          addExercisesForMuscleGroup(dzien, "chest", rep_range, ciezar, 3);
+        if (partie.includes("Barki"))
+          addExercisesForMuscleGroup(dzien, "shoulders", rep_range, ciezar, 3);
+
         workout_plan.push(dzien);
+        workout_day++;
       }
     }
   }
@@ -98,7 +175,11 @@ function displayPlan(plan, container) {
       day.exercises.forEach(function (exercise) {
         const szczegoly_cwiczenia = document.createElement("p");
         szczegoly_cwiczenia.textContent =
-          exercise.nazwa + " : " + exercise.reps + " reps, " + exercise.waga;
+          exercise.nazwa +
+          " : " +
+          exercise.reps +
+          " powtórzeń, " +
+          exercise.waga;
         szczegoly_treningu.appendChild(szczegoly_cwiczenia);
       });
 
